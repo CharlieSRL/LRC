2ea276535189fdcac1032361b908e87f37e11a41
CharlieSRL <50966953+CharlieSRL@users.noreply.github.com>
2023-05-25 11:14:46 -0600

    uploading
    uploading for testing purposes

---
+++ NFL_NBA/NFL.com_NBA.com/Action.c
@@ -0,0 +1,65 @@
+Action()
+{
+
+	web_url("device-desc.xml", 
+		"URL=http://192.168.100.38:8008/ssdp/device-desc.xml", 
+		"Resource=1", 
+		"RecContentType=application/xml", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		LAST);
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_header("Sec-Fetch-Mode", 
+		"no-cors");
+
+	web_add_header("Sec-Fetch-Dest", 
+		"empty");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"none");
+
+	/*Possible OAUTH authorization was detected. It is recommended to correlate the authorization parameters.*/
+
+	web_add_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_header("Sec-Fetch-Dest", 
+		"document");
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_add_header("sec-ch-ua", 
+		"\"Google Chrome\";v=\"113\", \"Chromium\";v=\"113\", \"Not-A.Brand\";v=\"24\"");
+
+	web_add_header("sec-ch-ua-mobile", 
+		"?0");
+
+	web_add_header("sec-ch-ua-platform", 
+		"\"Windows\"");
+
+	web_url("www.nfl.com", 
+		"URL=https://www.nfl.com/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+	
+	web_url("www.nba.com", 
+		"URL=https://www.nfl.com/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	return 0;
+}
